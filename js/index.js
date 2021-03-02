492864f48175cafb0cdcf4a2f88666a39b546198
candor-dennis <candordennis74@mail.com>
2021-03-02 23:15:08 +0100

    complete checkout page excluding payment system

--- js/index.js
+++ js/index.js
@@ -1,57 +1,78 @@
 
-function showItem() {
-
-    let item = document.getElementById("checkout");
-    let action =  item.style.display = 'block';
-
-    if (action = true) {
-        document.getElementById("banner").style.display = 'none'
-    } else {
-        console.log("i cannot hide")
-    }
-}
-
 
 let allItems = []
-
-function cartItems(id , name , price) {
-
-    // adding values on cart
-     let number = parseInt(document.getElementById("count").innerHTML);
-     number = number+1;
-     document.getElementById('count').innerHTML = number;
+let totalPrice = 0;
+let totalQuantity = 0;
 
 
-   data = {
-       id:id,
-       name :  name,
-       price : price,
-       quantity :1
-   };
+// cart function
+    function cartItems(id , name , price) {
 
+        // adding values on cart
+        let number = parseInt(document.getElementById("count").innerHTML);
+        number = number+1;
+        document.getElementById('count').innerHTML = number;
 
-   if (allItems.length == 0) {
-    allItems.push(data);
-    console.log(allItems);
+        data = {
+            id: id,
+            name :  name,
+            price : price,
+            quantity : 1
+        };
+    
 
-   } else {
-      for ( let i = 0 ;  i < allItems.length ; i++) {
-          if (allItems[i].id == data["id"]) {
-              alert("clicked");
-              let productData = {
-                  id : data["id"],
-                  name : data["name"],
-                  price: data["price"],
-                  quantity : parseInt(data["quantity"] +1 )
+        for ( let i = 0 ;  i < allItems.length ; i++) {
+            if (allItems[i].id == data.id) {
+                let productData = {
+                    id : data.id,
+                    name : data.name,
+                    price: allItems[i].price += price,
+                    quantity : +allItems[i].quantity + 1
                 };
                 allItems[i] = productData;
-                console.log(allItems);
-          }else{
-              alert("not clicked");
-          }
-          }
-           
-      } 
-   }
+                let productInfo = document.getElementById("content");
+                productInfo.innerHTML =
+                        `<div class = "d-flex justify-content-around> 
+                            <div class ="flex-item"> ${productData.name} </div>
+                            <div class ="flex-item"> ${data.price} </div>
+                            <div class ="flex-item"> ${data.quantity} </div>
+                      </div>    `
+                return;
+            }
+        }
+
+        allItems.push(data);
+    
+    } 
+
+    function showItem() {
+        let item = document.getElementById("checkout");
+        let action =  item.style.display = 'block';
+    
+        if (action = true) {
+            document.getElementById("banner").style.display = 'none';
+            document.getElementById("hide").style.display = 'none';
+        } else {
+            console.log("i cannot hide") 
+        };
+
+        for ( let i = 0 ;  i < allItems.length ; i++) {
+
+            totalPrice +=  +allItems[i].price
+            totalQuantity += +allItems[i].quantity;
+
+            // to display price
+            let priceContainer = document.getElementById("totalPrice");
+            priceContainer.innerHTML = totalPrice;
+
+            // to display quantity
+            let quantContainer = document.getElementById("totalQuantity");
+            quantContainer.innerHTML = totalQuantity;
+
+            return;
+        }
+      
+    }
+
 
 
