22674168f17e0918d97953c791fce72b9cb42abd
candor-dennis <candordennis74@mail.com>
2021-03-03 09:43:46 +0100

    checkout page modified

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@
 let allItems = []
 let totalPrice = 0;
 let totalQuantity = 0;
-
+const errorMessage = document.getElementById('error');
+const paymentForm = document.getElementById('paymentForm');
 
 // cart function
     function cartItems(id , name , price) {
@@ -47,6 +48,7 @@ let totalQuantity = 0;
         if (action = true) {
             document.getElementById("banner").style.display = 'none';
             document.getElementById("hide").style.display = 'none';
+            document.getElementById("hide-all").style.display = 'none'
         } else {
             console.log("i cannot hide") 
         };
@@ -69,11 +71,12 @@ let totalQuantity = 0;
         
         for (const item of allItems) {
             
-            let container = document.createElement("div");
-            container.style.border = "1px solid maroon"
+            let container = document.createElement("p");
+            // container.style.border = "1px solid maroon"
             container.style.margin = "10px"
+            container.style.padding = "10px"
             let productContent = document.createTextNode(`
-               Product Name:${item.name}, Product Price:${item.price}, Product Quantity: ${item.quantity} 
+               Product Name :${item.name}, Product Price :${item.price}, Product Quantity : ${item.quantity} 
             `);
             container.appendChild(productContent);
             var display = document.getElementById("content");
@@ -82,45 +85,59 @@ let totalQuantity = 0;
             return;
          }
 
-        
-
-      
     }
 
-    const paymentForm = document.getElementById('paymentForm');
+
+    
+
+
+
+
+    
 
     paymentForm.addEventListener("submit", payWithPaystack, false);
 
     function payWithPaystack(e) {
+        const userForm = document.forms[0];
+        const Fname =document.getElementById("checkoutFname").value;
+        const Lname =document.getElementById("checkoutLname").value;
+        const address = document.getElementById("checkoutAddress").value;
+
+        let nameCheck = /^[a-zA-Z\s]+$/;
+        let addressCheck = /^[a-zA-Z0-9\s]+$/;
+
+        if (Fname.trim() == '' || Fname.search(nameCheck) == -1) {
+            errorMessage.classList.remove('d-none');
+            errorMessage.innerHTML = 'Please enter a valid name';
+        
+        } else  if (Lname.trim() == '' || Lname.search(nameCheck) == -1) {
+            errorMessage.classList.remove('d-none');
+            errorMessage.innerHTML = 'Please enter a valid name';
+        }
+            
+        else if (address.trim() == '' || address.search(addressCheck) == -1) {
+            errorMessage.classList.remove('d-none');
+            errorMessage.innerHTML = 'Please enter a valid address';
+        } else {
+                errorMessage.classList.add('d-none');
+        }
+        // form validation
 
         e.preventDefault();
-
         let handler = PaystackPop.setup({
-
         key: 'pk_test_xxxxxxxxxx', // Replace with your public key
-
         email: document.getElementById("email-address").value,
-
         amount: document.getElementById("amount").value * 100,
-
         ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
 
         // label: "Optional string that replaces customer email"
 
         onClose: function(){
-
         alert('Window closed.');
-
             },
-
             callback: function(response){
-
             let message = 'Payment complete! Reference: ' + response.reference;
-
-        
-
             alert(message);
-
             }
 
         });
